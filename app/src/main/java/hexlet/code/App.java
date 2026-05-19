51029f74e1af233a64f1484529cb2e9fef638ae3
snevg04 <evg.sneg.117@yandex.ru>
2026-05-19 12:57:16 +1000

    add Javalin-app to project

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,19 @@
 package hexlet.code;
 
+import io.javalin.Javalin;
+
 public class App {
     public static void main(String[] args) {
+        var app = getApp();
+        app.start(7070);
+    }
 
+    public static Javalin getApp() {
+        var app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
+            config.routes.get("/", ctx -> ctx.result("Hello World"));
+        });
+
+        return app;
     }
 }
