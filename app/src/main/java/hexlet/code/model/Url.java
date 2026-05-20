082b1ce1fc250cf498a235df90667f938f9380fa
snevg04 <evg.sneg.117@yandex.ru>
2026-05-20 22:20:00 +1000

    create Url model

--- app/src/main/java/hexlet/code/model/Url.java
+++ app/src/main/java/hexlet/code/model/Url.java
@@ -1,4 +1,14 @@
 package hexlet.code.model;
 
+import lombok.Getter;
+import lombok.AllArgsConstructor;
+
+import java.sql.Timestamp;
+
+@AllArgsConstructor
+@Getter
 public class Url {
+    private long id;
+    private String name;
+    private Timestamp createdAt;
 }
