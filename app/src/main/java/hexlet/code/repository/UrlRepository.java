f3e42f36c4c4bca22beb8ac85920889e393fd2ac
snevg04 <evg.sneg.117@yandex.ru>
2026-05-22 21:52:07 +1000

    add find and findByName to UrlRepository

--- app/src/main/java/hexlet/code/repository/UrlRepository.java
+++ app/src/main/java/hexlet/code/repository/UrlRepository.java
@@ -4,6 +4,7 @@ import hexlet.code.model.Url;
 
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Optional;
 
 public class UrlRepository extends BaseRepository {
     public static void save(Url url) throws SQLException {
@@ -20,6 +21,53 @@ public class UrlRepository extends BaseRepository {
             } else {
                 throw new SQLException("DB has not returned an id after saving an entity");
             }
+        }
+    }
+
+    public static Optional<Url> find(Long id) throws SQLException {
+        String sql = "SELECT * FROM urls WHERE id = ?";
+
+        try (var conn = dataSource.getConnection();
+            var preparedStatement = conn.prepareStatement(sql)) {
+
+            preparedStatement.setLong(1,id);
+
+            var resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                var name = resultSet.getString("name");
+                var createdAt = resultSet.getTimestamp("created_at");
+
+                var url = new Url(name, createdAt);
+                url.setId(id);
+
+                return Optional.of(url);
+            }
+
+            return Optional.empty();
+        }
+    }
+
+    public static Optional<Url> findByName(String name) throws SQLException {
+        String sql = "SELECT * FROM urls WHERE name = ?";
+
+        try (var conn = dataSource.getConnection();
+            var preparedStatement = conn.prepareStatement(sql)) {
+            preparedStatement.setString(1, name);
+            var resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                var actualName = resultSet.getString("name");
+                var id = resultSet.getLong("id");
+                var createdAt = resultSet.getTimestamp("created_at");
+
+                var url = new Url(actualName, createdAt);
+                url.setId(id);
+
+                return Optional.of(url);
+            }
+
+            return Optional.empty();
         }
     }
 }
