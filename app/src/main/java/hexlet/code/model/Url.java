e3045b55e6a511e11416669a42ad4a680a32872c
snevg04 <evg.sneg.117@yandex.ru>
2026-05-22 14:33:37 +1000

    add url model

--- app/src/main/java/hexlet/code/model/Url.java
+++ app/src/main/java/hexlet/code/model/Url.java
@@ -2,13 +2,19 @@ package hexlet.code.model;
 
 import lombok.Getter;
 import lombok.AllArgsConstructor;
+import lombok.Setter;
 
 import java.sql.Timestamp;
 
-@AllArgsConstructor
 @Getter
+@Setter
 public class Url {
     private long id;
     private String name;
     private Timestamp createdAt;
+
+    public Url(String name, Timestamp createdAt) {
+        this.name = name;
+        this.createdAt = createdAt;
+    }
 }
