cfb7f0b9ba7a427a72abe45d8b99df0a1efb019e
snevg04 <evg.sneg.117@yandex.ru>
2026-05-23 22:34:15 +1000

    add routing to app

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,8 +2,9 @@ package hexlet.code;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
-import hexlet.code.dto.BasePage;
+import hexlet.code.controller.UrlController;
 import hexlet.code.repository.BaseRepository;
+import hexlet.code.util.NamedRoutes;
 import io.javalin.Javalin;
 import gg.jte.ContentType;
 import gg.jte.TemplateEngine;
@@ -11,14 +12,20 @@ import io.javalin.rendering.template.JavalinJte;
 import gg.jte.resolve.ResourceCodeResolver;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.sql.SQLException;
 import java.util.stream.Collectors;
 
-import static io.javalin.rendering.template.TemplateUtil.model;
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         var app = getApp();
         var port = Integer.parseInt(System.getenv().getOrDefault("PORT", "7070"));
+
+        app.get(NamedRoutes.rootPath(), UrlController::index);
+        app.post(NamedRoutes.urlsPath(), UrlController::create);
+        app.get(NamedRoutes.urlsPath(), UrlController::list);
+        app.get(NamedRoutes.urlPath("{id}"), UrlController::show);
+
         app.start(port);
     }
 
@@ -29,7 +36,7 @@ public class App {
         return templateEngine;
     }
 
-    public static Javalin getApp() {
+    public static Javalin getApp() throws SQLException {
         var hikariConfig = new HikariConfig();
         var jdbcUrl = System.getenv()
                 .getOrDefault("JDBC_DATABASE_URL", "jdbc:h2:mem:project;DB_CLOSE_DELAY=-1");
@@ -44,17 +51,16 @@ public class App {
         var sql = new BufferedReader(new InputStreamReader(url))
                 .lines().collect(Collectors.joining("\n"));
 
+        try (var connection = dataSource.getConnection();
+            var statement = connection.createStatement()) {
+            statement.execute(sql);
+        }
+
         BaseRepository.dataSource = dataSource;
-
-
 
         var app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
             config.fileRenderer(new JavalinJte(createTemplateEngine()));
-            config.routes.get("/", ctx -> {
-                var page = new BasePage();
-                ctx.render("index.jte", model("page", page));
-            });
         });
 
         return app;
