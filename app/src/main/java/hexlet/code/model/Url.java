dcdcf671792847549b0c252e10e77bd08ad6fad7
snevg04 <evg.sneg.117@yandex.ru>
2026-05-23 22:31:58 +1000

    update url model

--- app/src/main/java/hexlet/code/model/Url.java
+++ app/src/main/java/hexlet/code/model/Url.java
@@ -1,19 +1,39 @@
 package hexlet.code.model;
 
-import lombok.Getter;
-import lombok.Setter;
-
 import java.sql.Timestamp;
 
-@Getter
-@Setter
+
 public class Url {
-    private long id;
+    private Long id;
     private String name;
     private Timestamp createdAt;
 
     public Url(String name, Timestamp createdAt) {
         this.name = name;
+        this.createdAt = createdAt;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
         this.createdAt = createdAt;
     }
 
