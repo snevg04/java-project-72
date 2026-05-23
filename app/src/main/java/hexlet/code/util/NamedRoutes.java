0d69d5091553cf6960bbfab2a66f7b2592c7dcc5
snevg04 <evg.sneg.117@yandex.ru>
2026-05-23 22:32:28 +1000

    update NamedRoutes

--- app/src/main/java/hexlet/code/util/NamedRoutes.java
+++ app/src/main/java/hexlet/code/util/NamedRoutes.java
@@ -11,6 +11,10 @@ public class NamedRoutes {
     }
 
     public static String urlPath(Long id) {
+        return urlPath(String.valueOf(id));
+    }
+
+    public static String urlPath(String id) {
         return "/urls/" + id;
     }
 }
