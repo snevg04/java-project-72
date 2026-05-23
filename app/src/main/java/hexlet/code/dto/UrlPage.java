7658d7036eb3a430efbc26c0beb4682ae19ca719
snevg04 <evg.sneg.117@yandex.ru>
2026-05-23 22:31:45 +1000

    update dto

--- app/src/main/java/hexlet/code/dto/UrlPage.java
+++ app/src/main/java/hexlet/code/dto/UrlPage.java
@@ -1,15 +1,15 @@
 package hexlet.code.dto;
 
 import hexlet.code.model.Url;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
 
-import java.sql.Timestamp;
-
-@AllArgsConstructor
-@Getter
-@Setter
 public class UrlPage extends BasePage {
     private Url url;
+
+    public UrlPage(Url url) {
+        this.url = url;
+    }
+
+    public Url getUrl() {
+        return url;
+    }
 }
--- app/src/main/java/hexlet/code/dto/UrlsPage.java
+++ app/src/main/java/hexlet/code/dto/UrlsPage.java
@@ -1,15 +1,17 @@
 package hexlet.code.dto;
 
 import hexlet.code.model.Url;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
 
 import java.util.List;
 
-@AllArgsConstructor
-@Getter
-@Setter
 public class UrlsPage extends BasePage {
     private List<Url> urls;
+
+    public UrlsPage(List<Url> urls) {
+        this.urls = urls;
+    }
+
+    public List<Url> getUrls() {
+        return urls;
+    }
 }
