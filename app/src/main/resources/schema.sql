fb3ef53febe319f9b34d2b48630da20ec69d084f
snevg04 <evg.sneg.117@yandex.ru>
2026-05-26 13:00:28 +1000

    update schema.sql

--- app/src/main/resources/schema.sql
+++ app/src/main/resources/schema.sql
@@ -1,7 +1,18 @@
+DROP TABLE IF EXISTS url_checks;
 DROP TABLE IF EXISTS urls;
 
 CREATE TABLE urls (
     id SERIAL PRIMARY KEY,
     name varchar(255),
     created_at timestamp
-)
+);
+
+CREATE TABLE url_checks (
+    id SERIAL PRIMARY KEY,
+    url_id bigint REFERENCES urls(id) ON DELETE CASCADE,
+    status_code int,
+    h1 varchar(200),
+    title varchar(200),
+    description varchar(200),
+    created_at timestamp
+);
