e55447da16d5343687d596b7b43ef4f7bd6c47fb
snevg04 <evg.sneg.117@yandex.ru>
2026-05-26 13:00:36 +1000

    update UrlPage

--- app/src/main/java/hexlet/code/dto/UrlPage.java
+++ app/src/main/java/hexlet/code/dto/UrlPage.java
@@ -1,15 +1,25 @@
 package hexlet.code.dto;
 
 import hexlet.code.model.Url;
+import hexlet.code.model.UrlCheck;
+
+import java.util.List;
 
 public class UrlPage extends BasePage {
     private Url url;
+    private List<UrlCheck> checks;
 
-    public UrlPage(Url url) {
+    public UrlPage(Url url, List<UrlCheck> checks) {
         this.url = url;
+        this.checks = checks;
     }
 
     public Url getUrl() {
         return url;
     }
+
+    public List<UrlCheck> getChecks() {
+        return checks;
+    }
+
 }
